43a71d9f9421b06cf90a9608a32a2b192e4f5999
Murilo F Schaefer <murilo.schaefer@gmail.com>
2023-03-29 08:10:59 -0300

    fix: model json restrictions
    change it to allow only tecidos and malhas (MINOR)

--- webapp/src/system/modules/Composer/components/editableFields/MaterialSelector.tsx
+++ webapp/src/system/modules/Composer/components/editableFields/MaterialSelector.tsx
@@ -2,11 +2,11 @@ import { useCallback } from "react";
 
 import useModule from "@kernel/hooks/useModule";
 
+import { IGraphModule } from "@kernel/modules/Graphs";
 import { IMaterialsModule } from "@system/modules/Materials";
 
 import { MaterialNode, MaterialUsageNode } from "../../store/composition/state";
 import useComposition from "../../hooks/useComposition";
-import { IGraphModule } from "@kernel/modules/Graphs";
 
 const MaterialSelector = ({
   graphId,
