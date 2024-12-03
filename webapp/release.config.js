6fb2767a6624fc77f596dcbe40d47b936767b7c3
Murilo Schaefer <murilo.schaefer@gmail.com>
2024-12-02 23:53:12 -0300

    chore(ci/cd): update package json version before commiting

--- webapp/release.config.js
+++ webapp/release.config.js
@@ -4,9 +4,15 @@ module.exports = {
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
     [
+      "@semantic-release/exec",
+      {
+        "prepareCmd": "npm version ${nextRelease.version}",
+      }
+    ],
+    [
       "@semantic-release/git",
       {
-        assets: ["dist/*"],
+        assets: ["dist/*", './package.json', './package-lock.json'],
         message:
           "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
       },
