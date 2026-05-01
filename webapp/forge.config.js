156306ec78bb30c4ab188393ac587dc66948619f
Murilo Schaefer <murilo.schaefer@gmail.com>
2026-04-30 23:33:52 -0300

    chore: improve windows release

--- webapp/forge.config.js
+++ webapp/forge.config.js
@@ -3,12 +3,19 @@
 
 module.exports = {
   packagerConfig: {
+    asar: true,
     ignore: [
       /^\/src/,
       /(.eslintrc.json)|(.gitignore)|(electron.vite.config.ts)|(forge.config.cjs)|(tsconfig.*)/,
       // Exclude prebuilt native modules for non-target platforms.
       // rpmbuild's brp-strip fails on .bare files for foreign architectures.
       /node_modules\/.+\/prebuilds\/(android|linux-arm)/,
+      // Strip docs, tests, and source maps from node_modules to shrink the payload Squirrel compresses.
+      /node_modules\/.+\/(test|tests|__tests__|example|examples|docs|doc)(\/|$)/,
+      /node_modules\/.+\.(md|markdown|ts|map|d\.ts)$/,
+      /node_modules\/.+\/(LICENSE|LICENCE|CHANGELOG|HISTORY|AUTHORS|CONTRIBUTORS)(\..*)?$/i,
+      /node_modules\/.+\/\.(github|vscode|idea|nyc_output|circleci)(\/|$)/,
+      /node_modules\/.+\/(\.editorconfig|\.eslintrc.*|\.prettierrc.*|\.babelrc.*|\.npmignore|\.travis\.yml|\.gitattributes)$/,
     ],
   },
   rebuildConfig: {},
